416a2d4f9246a82a29daadcee16c64bde28e1111
Jason Gross <jgross@mit.edu>
2020-02-28 19:13:42 -0500

    Fix todo.svg size display

--- etc/makesvg.sh
+++ etc/makesvg.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 lines="$(cat -)"
-nlines=$(( ( 11 + $(echo "$lines" | wc -l) * 12 ) / 10 ))
+nlines="$(( ( 11 + $(echo "$lines" | wc -l) * 12 ) / 10 ))em"
 # https://www.unix.com/shell-programming-and-scripting/32049-line-maximum-no-characters.html
-nchars="$(echo "$lines" | awk ' length > max { max=length;row=NR } END{ print max}')"
+nchars="$(echo "$lines" | awk ' length > max { max=length;row=NR } END{ print max}')em"
 nchars="100%"
 
-echo '<svg xmlns="http://www.w3.org/2000/svg" width="'"$nchars"'em" height="'"$nlines"'em"><text x="0" y="0">'
+echo '<svg xmlns="http://www.w3.org/2000/svg" width="'"$nchars"'" height="'"$nlines"'"><text x="0" y="0">'
 echo "$lines" | cat -v | sed s'|^\(.*\)$|<tspan x="0" dy="1.2em">\1</tspan>|g'
 echo '</text></svg>'

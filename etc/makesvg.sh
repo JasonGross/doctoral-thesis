f3cab7c7eb3df0bd82df89bc404e6aaeab4add12
Jason Gross <jgross@mit.edu>
2020-03-03 23:01:28 -0500

    Update makesvg.sh

--- etc/makesvg.sh
+++ etc/makesvg.sh
@@ -11,5 +11,5 @@ nlines="$(( ( 11 + $(echo "$lines" | wc -l) * 12 ) / 10 ))em"
 nchars="$(echo "$lines" | (cd "$DIR" && ./stringwidth.py --max --int))em"
 
 echo '<svg xmlns="http://www.w3.org/2000/svg" width="'"$nchars"'" height="'"$nlines"'"><text x="0" y="0">'
-echo "$lines" | cat -v | sed s'/</&lt;/g' | sed s'/>/&gt;/g' | sed s'|^\(.*\)$|<tspan x="0" dy="1.2em">\1</tspan>|g'
+echo "$lines" | cat -v | sed s'/</\&lt;/g' | sed s'/>/\&gt;/g' | sed s'|^\(.*\)$|<tspan x="0" dy="1.2em">\1</tspan>|g'
 echo '</text></svg>'

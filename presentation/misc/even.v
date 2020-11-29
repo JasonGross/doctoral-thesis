400e7aeed1a855d578b4be02c709fb2bc56b436d
Jason Gross <jgross@mit.edu>
2020-11-29 18:47:20 -0500

    WIP

--- presentation/misc/even.v
+++ presentation/misc/even.v
@@ -1,6 +1,6 @@
 Inductive is_even : nat -> Prop :=
 | zero_even : is_even 0
-| two_plus_even {n} : is_even n -> is_even (S (S n)).
+| two_plus_even n : is_even n -> is_even (S (S n)).
 Inductive parity := even | odd.
 Definition flip_parity p
   := match p with even => odd | odd => even end.
@@ -11,4 +11,14 @@ Fixpoint parity_of (n : nat) : parity :=
   end.
 Lemma parity_of_correct : forall n, parity_of n = even -> is_even n.
 Admitted.
+Goal is_even 9002. vm_compute; do 10 constructor. Show Proof. Abort.
+Check (two_plus_even 9000
+   (two_plus_even 8998
+      (two_plus_even 8996
+         (two_plus_even 8994
+            (two_plus_even 8992
+               (two_plus_even 8990
+                  (two_plus_even 8988
+                     (two_plus_even 8986
+                        (two_plus_even 8984 (two_plus_even 8982 ?[Goal])))))))))).
 Goal is_even 9002. apply parity_of_correct; vm_compute; reflexivity. Qed.
